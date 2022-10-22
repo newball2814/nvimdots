0a1a3c2b3e9173a24f337f8f85bd1b7a22572cdc
newball2814 <thangtanpro@gmail.com>
2022-10-22 19:15:46 +0700

    First coomit

---
+++ init.lua
@@ -0,0 +1,9 @@
+require "user.options"              -- general settings
+require "user.plugins"              -- plugins
+require "user.colorscheme"          -- colorscheme
+require "user.cmp"                  -- code completion
+require "user.lsp.nvim-lspconfig"   -- LSP server
+require "user.lsp.mason"            -- Manager for LSP config
+require "user.telescope"            -- fuzzy finder
+require "user.keymaps"              -- custom keybindings
+require "user.treesitter"           -- syntax highlighting
